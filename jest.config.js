4275a20938cc39a895ec13fd5f7b90134d2be5e0
Tomortec <everything@tomortec.com>
2023-10-16 20:21:44 +0800

    test: add Jest to project test: remove unnecessary testing assets from repo

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFiles: ["<rootDir>/jest.setup.js"]
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const path = require('path');
+const webpack = require("webpack");
 const MockjsWebpackPlugin = require('mockjs-webpack-plugin');
 
 module.exports = merge(common, {
@@ -12,16 +13,24 @@ module.exports = merge(common, {
         static: "./dist",
         hot: true,
         proxy: {
-            "/adminApi": "http://localhost:3000/"
+            "/admin": "http://localhost:3000/",
+            "/api": "http://localhost:3001/"
         }
     },
     output: {
         publicPath: "/"
     },
     plugins: [
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify("development")
+        }),
         new MockjsWebpackPlugin({
             path: path.join(__dirname, "./admin/__mock__"),
             port: 3000
+        }),
+        new MockjsWebpackPlugin({
+            path: path.join(__dirname, "./src/__mock__"),
+            port: 3001
         })
     ]
 });
